be2007ac047fbb2ccf6675d496a792e66a77bf4b
hideyosi <hideyosi@70df1f67-1e22-0410-bc7b-a348b552bfc5>
2009-12-10 19:05:12 +0000

    ファイル／フォルダを削除
    git-svn-id: http://svn.osdn.jp/svnroot/osask@8 70df1f67-1e22-0410-bc7b-a348b552bfc5

--- OSASK/trunk/sgg00lib/makelib.bat
+++
@@ -1,6 +0,0 @@
-ml /c /coff execcmd.asm
-lcc init.c     -Ih:\osask\pioneer0\lccinc -O
-lcc fwinman.c -Ih:\osask\pioneer0\lccinc -O
-lcc fpokon.c -Ih:\osask\pioneer0\lccinc -O
-lcc fdebug.c -Ih:\osask\pioneer0\lccinc -O
-lcclib -OUT:sysgg00.lib @sysgg00.opt
