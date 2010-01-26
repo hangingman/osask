37dca76f9bef35b8872d5920329d8d2105ce29c7
hideyosi <hideyosi@70df1f67-1e22-0410-bc7b-a348b552bfc5>
2010-01-26 07:33:08 +0000

    一通りmakeは成功した。とりあえずは。
    git-svn-id: http://svn.osdn.jp/svnroot/osask@135 70df1f67-1e22-0410-bc7b-a348b552bfc5

--- 28GO/28GO_K/branches/K_0033_test/cpy/make.bat
+++ 28GO/28GO_K/branches/K_0033_test/cpy/make.bat
@@ -1,6 +1,2 @@
-rem ..\z_tools\make.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
+..\z_tools\make.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
 
-..\z_tools\cpp0.exe -P -I. -o cpy.ias cpy.ask
-..\z_tools\aska.exe cpy.ias cpy.3as
-..\z_tools\efg01.exe ../z_tools/naskcnv0.g01 -l -s in:cpy.3as out:cpy.nas
-..\z_tools\efg01.exe ../z_tools/nask.g01 in:cpy.nas out:cpy.org lst:cpy.lst
