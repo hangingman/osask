77c8230095a6cfe467447a04fc9d7aaf42a96f50
hiroyuki nagata <newserver002@gmail.com>
2014-10-26 20:37:09 +0900

    Set version string for aska

--- 28GO/aska/main.cpp
+++ 28GO/aska/main.cpp
@@ -1,18 +1,33 @@
 #include "parser.hpp"
 
-void usage(void){
-	puts("Asuka Ver1.1 by hideyosi");
-	puts("\tUsage: Asuka <InputFile> <OutputFile>");
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+const std::string version  = std::string("aska ver1.1 for " PACKAGE_STRING);
+const std::string usagestr = "\tUsage: aska <InputFile> <OutputFile>";
+
+void usage(void)
+{
+     std::cout << version  << std::endl;
+     std::cout << usagestr << std::endl;
 }
 
-int main(int argc, char* argv[]){
-	Parser	parser;
-	if(argc < 2){
-		usage();
-	}else if(argc < 3){
-		return (int) parser.Compile(std::string(argv[1]));
-	}else{
-		return (int) parser.Compile(std::string(argv[1]), std::string(argv[2]));
-	}
-	return 0;
+int main(int argc, char* argv[])
+{
+     Parser parser;
+     if(argc < 2)
+     {
+	  usage();
+     } 
+     else if(argc < 3)
+     {
+	  return (int) parser.Compile(std::string(argv[1]));
+     }
+     else
+     {
+	  return (int) parser.Compile(std::string(argv[1]), std::string(argv[2]));
+     }
+
+     return 0;
 }
