9f7a719a388fc73d4e6ed4e9c333002c1e6920c4
hiroyuki nagata <newserver002@gmail.com>
2016-04-10 00:30:20 +0900

    [master] Disable silent-rules during building in the CI

--- cross-i686-w64-mingw32.sh
+++ cross-i686-w64-mingw32.sh
@@ -6,5 +6,6 @@
 ./configure --prefix=/usr/i686-w64-mingw32           \
 --build=x86_64-unknown-linux-gnu                     \
 --host=i686-w64-mingw32                              \
---target=i686-w64-mingw32
+--target=i686-w64-mingw32                            \
+--disable-silent-rules
 make
--- cross-x86_64-w64-mingw32.sh
+++ cross-x86_64-w64-mingw32.sh
@@ -6,5 +6,6 @@
 ./configure --prefix=/usr/x86_64-w64-mingw32           \
 --build=x86_64-unknown-linux-gnu                       \
 --host=x86_64-w64-mingw32                              \
---target=x86_64-w64-mingw32
+--target=x86_64-w64-mingw32                            \
+--disable-silent-rules
 make
